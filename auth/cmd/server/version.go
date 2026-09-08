c40de820bf3b0777c8b44e0ebbb107deb674455c
Ronny Trommer <ronny@no42.org>
2026-09-08 10:55:13 +0200

    chore: bump versions to 0.6.2-rc post v0.6.1 (#235)
    Post-release bump so preview builds on `main` publish `0.6.2-rc` and
    cannot overwrite the released `0.6.1` images.
    
    Signed-off-by: Ronny Trommer <ronny@no42.org>

--- auth/cmd/server/version.go
+++ auth/cmd/server/version.go
@@ -7,4 +7,4 @@ package main
 
 // version is the current application version. On main this carries a -rc suffix;
 // drop the suffix when cutting a release tag, then bump to the next -rc version.
-const version = "0.6.1"
+const version = "0.6.2-rc"
